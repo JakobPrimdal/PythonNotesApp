0baad8e3c9e7340dbe03e494c95e3d629b32195b
Jakob Primdal <jakobprimdal@gmail.com>
2026-07-12 17:46:43 +0200

    Added test for use case delete note

--- test/application/test_delete_note.py
+++ test/application/test_delete_note.py
@@ -1,5 +1,8 @@
+from uuid import uuid4
+
 from application.use_cases.note.CreateNote import CreateNote
 from application.use_cases.note.DeleteNote import DeleteNote
+from domain.Note import Note
 from domain.interfaces.INoteRepository import INoteRepository
 from test.fakes.in_memory_note_repository import InMemoryNoteRepository
 
@@ -8,8 +11,7 @@ def test_delete_note_with_valid_uuid_succeeds() -> None:
     # Arrange
     repository: INoteRepository = InMemoryNoteRepository()
 
-    use_case_create = CreateNote(note_repository=repository)
-    note = use_case_create.execute(title="title", content="content")
+    note = Note.create(title="title", content="content")
 
     use_case_delete = DeleteNote(note_repository=repository)
 
@@ -18,3 +20,16 @@ def test_delete_note_with_valid_uuid_succeeds() -> None:
 
     # Assert
     assert repository.get_all() == []
+
+def test_delete_note_with_unknow_uuid_does_not_raise() -> None:
+    # Arrange
+    repository: INoteRepository = InMemoryNoteRepository()
+    note = Note.create(title="title", content="content")
+    repository.save(note)
+    use_case = DeleteNote(note_repository=repository)
+
+    # Act
+    use_case.execute(note_uuid=uuid4())
+
+    # Assert
+    assert repository.get_all() == [note]
