f57f517f5f410d77db4dcb07a1488acd03fabfff
Jakob Primdal <jakobprimdal@gmail.com>
2026-07-18 15:10:26 +0200

    Refactor: Delete note/folder use cases can now throw Note/FolderNoteFoundError

--- application/use_cases/folder/DeleteFolder.py
+++ application/use_cases/folder/DeleteFolder.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from domain.errors.FolderNotFoundError import FolderNotFoundError
 from domain.interfaces.IFolderRepository import IFolderRepository
 
 
@@ -11,4 +12,7 @@ class DeleteFolder:
         self._folder_repository = folder_repository
 
     def execute(self, folder_uuid: UUID) -> None:
+        folder = self._folder_repository.get_by_uuid(folder_uuid)
+        if folder is None:
+            raise FolderNotFoundError(f"Folder with uuid {folder_uuid} not found")
         self._folder_repository.delete(folder_uuid)
--- application/use_cases/note/DeleteNote.py
+++ application/use_cases/note/DeleteNote.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from domain.errors.NoteNotFoundError import NoteNotFoundError
 from domain.interfaces.INoteRepository import INoteRepository
 
 
@@ -8,4 +9,7 @@ class DeleteNote:
         self._note_repository = note_repository
 
     def execute(self, note_uuid: UUID) -> None:
+        note = self._note_repository.get_by_uuid(note_uuid)
+        if note is None:
+            raise NoteNotFoundError(f"Note with uuid {note_uuid} not found")
         self._note_repository.delete(note_uuid)
