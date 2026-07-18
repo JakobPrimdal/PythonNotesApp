a1105d283121b7549811b952783c6274ac0b276a
Jakob Primdal <jakobprimdal@gmail.com>
2026-07-18 15:06:39 +0200

    Refactor: UpdateFolder now checks for duplicate names

--- application/use_cases/folder/UpdateFolder.py
+++ application/use_cases/folder/UpdateFolder.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
 from domain.Folder import Folder
+from domain.errors.DuplicateFolderNameError import DuplicateFolderNameError
 from domain.errors.FolderNotFoundError import FolderNotFoundError
 from domain.interfaces.IFolderRepository import IFolderRepository
 
@@ -13,6 +14,10 @@ class UpdateFolder:
         folder = self._folder_repository.get_by_uuid(folder_uuid)
         if folder is None:
             raise FolderNotFoundError(f"Folder with uuid {folder_uuid} not found")
+
+        other_folders = [f for f in self._folder_repository.get_all() if f.id != folder_uuid]
+        if any(f.name == new_name for f in other_folders):
+            raise DuplicateFolderNameError(f"Folder with name {new_name} already exists")
 
         folder.rename(new_name)
         self._folder_repository.save(folder)
